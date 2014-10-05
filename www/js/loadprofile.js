9225ce8feb1cff46bfe6495bae0e002e159cf873
David Naylor <dnaylor@cs.cmu.edu>
2014-10-05 14:52:50 -0400

    Added web files

---
+++ www/js/loadprofile.js
@@ -0,0 +1,389 @@
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+
+function pretty(string)
+{
+	var map = {
+		'xml':'XML',
+		'css':'CSS',
+		'html':'HTML',
+		'json':'JSON',
+	};
+
+	if (string in map)
+		return map[string];
+	else
+    	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function dict_to_pie_data(dict) {
+	pie_data = [];
+	for (key in dict) {
+		pie_data.push([pretty(key), dict[key]]);
+	}
+	return pie_data;
+}
+
+/* makes two pie data arrays with the same data labels
+   (e.g., to make sure the same label gets the same color in 2 charts) */
+function dicts_to_pie_data(dict1, dict2) {
+	pie_data = [[],[]];  // we give back two arrays
+
+	for (key in dict1) {
+		pie_data[0].push([pretty(key), dict1[key]]);
+		if (key in dict2)
+			pie_data[1].push([pretty(key), dict2[key]]);
+		else
+			pie_data[1].push([pretty(key), 0]);
+	}
+	
+	// now get any leftover keys in dict 2 but not 1
+	for (key in dict2) {
+		if (!(key in dict1)) {
+			pie_data[1].push([pretty(key), dict2[key]]);
+			pie_data[0].push([pretty(key), 0]);
+		}
+	}
+	return pie_data;
+}
+
+function make_pie_chart(id, title, data, tooltip_postfix) {
+    $(id).highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+			margin: [25, 25, 40, 25],
+        },
+        title: {
+            text: title
+        },
+        tooltip: {
+    	    pointFormat: '{series.name}: <b>{point.percentage:.1f}% ({point.y} ' + tooltip_postfix + ')</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+				showInLegend: true,
+            }
+        },
+        series: [{
+            type: 'pie',
+            data: data,
+        }]
+    });
+}
+
+
+function dicts_to_stacked_bar_data(dicts) {
+	var segments = [];  // color segment names
+	var series = [];
+
+	// figure out the complete list of categories
+	for (i=0; i < dicts.length; i++) {
+		// grab segment names if we don't have them already
+		for (var key in dicts[i]) {
+			if (segments.indexOf(key) == -1)
+				segments.push(key);
+		}
+	}
+
+	// get values
+	for (i=0; i < segments.length; i++) {
+		var data = [];
+		for (j=0; j < dicts.length; j++) {
+			if (segments[i] in dicts[j])
+				data.push(dicts[j][segments[i]]);
+			else
+				data.push(0);
+		}
+		
+		series.push({ "name":pretty(segments[i]), "data":data});
+	}
+
+	return series;
+}
+
+/*
+ * categories are the colored bar segments
+ * series is an array of associative arrays w/ name and data entries
+ */
+function make_stacked_bar(id, title, ylabel, tooltip_postfix, stacking, categories, series) {
+	$(function () {
+        $(id).highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: title
+            },
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: ylabel
+                }
+            },
+			tooltip: {
+				followPointer: true,
+    	    	pointFormat: '{series.name}: <b>{point.y} ' + tooltip_postfix + ' ({point.percentage:.1f}%)</b>'
+			},
+            legend: {
+                reversed: true
+            },
+            plotOptions: {
+                series: {
+                    stacking: stacking
+                }
+            },
+                series: series,
+        });
+    });
+}
+
+
+/*
+ * metric is either 'count' or 'size'
+ */
+function load_object_type_breakdown(metric, stacking) {
+	// Get the path to the site's JSON profile
+	// TODO: refactor this
+	var profile_path = './alexa-top500/';
+	var params = getSearchParameters();
+	if (params.hasOwnProperty("site")) {
+		profile_path += params["site"] + ".json";
+	} else {
+		return -1;
+	}
+
+	$.getJSON(profile_path, function(data) {
+		/*
+		 * Object Type Bars
+		 */
+		if (metric == 'count') {
+			var series = dicts_to_stacked_bar_data([data["http-profile"]["num-objects-by-type"],
+													data["https-profile"]["num-objects-by-type"]]);
+			var ylabel = stacking == 'normal' ? 'Number of Objects' : 'Percent of Objects';
+			make_stacked_bar('#object-types', null, ylabel, 'objects', stacking, ['HTTP', 'HTTPS'], series);
+		} else if (metric == 'size') {
+			var series = dicts_to_stacked_bar_data([data["http-profile"]["num-bytes-by-type"],
+													data["https-profile"]["num-bytes-by-type"]]);
+			var ylabel = stacking == 'normal' ? 'Number of Bytes' : 'Percent of Bytes';
+			make_stacked_bar('#object-types', null, ylabel, 'bytes', stacking, ['HTTP', 'HTTPS'], series);
+
+		}
+		
+		/*
+		 * Object Type Pie
+		 */
+		//obj_type_data = dicts_to_pie_data(data["http-profile"]["num-objects-by-type"],
+		//									data["https-profile"]["num-objects-by-type"]);
+		//make_pie_chart('#http-object-types', 'HTTP Site', obj_type_data[0], 'objects');
+		//make_pie_chart('#https-object-types', 'HTTPS Site', obj_type_data[1], 'objects');
+
+	});
+}
+
+
+$(function () {
+	// Get the path to the site's JSON profile
+	var profile_path = './alexa-top500/';
+	var params = getSearchParameters();
+	if (params.hasOwnProperty("site")) {
+		profile_path += params["site"] + ".json";
+	} else {
+		return -1;
+	}
+
+	$.getJSON(profile_path, function(data) {
+
+		/*
+		 * Site URL
+		 */
+		document.getElementById("site-url").innerHTML = data["base-url"];
+		document.getElementById("site-breadcrumb").innerHTML = data["base-url"];
+
+
+		/*
+		 * Bar charts
+		 */
+		$('#basic-stats').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Basic Statistics'
+            },
+            xAxis: {
+                categories: ['Number of Objects', 'Total Size (MB)', 'Number of Hosts', 'Number of Connections'],
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: null
+                }
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'HTTP',
+                data: [data["http-profile"]["num-objects"], data["http-profile"]["num-bytes"]/1000000.0, data["http-profile"]["num-hosts"], data["http-profile"]["num-tcp-handshakes"]],
+            }, {
+                name: 'HTTPS',
+                data: [data["https-profile"]["num-objects"], data["https-profile"]["num-bytes"]/1000000.0, data["https-profile"]["num-hosts"], data["https-profile"]["num-tcp-handshakes"]],
+            }]
+        });
+
+
+
+
+
+		/*
+		 * HTTP Protocol Counts Pie
+		 */
+    	$('#http-protocol-counts').highcharts({
+    	    chart: {
+    	        plotBackgroundColor: null,
+    	        plotBorderWidth: null,
+    	        plotShadow: false,
+				margin: [25, 25, 25, 25],
+    	    },
+    	    title: {
+    	        text: 'HTTP Site'
+    	    },
+    	    tooltip: {
+    		    pointFormat: '{series.name}: <b>{point.percentage:.1f}% ({point.y} objects)</b>'
+    	    },
+    	    plotOptions: {
+    	        pie: {
+    	            allowPointSelect: true,
+    	            cursor: 'pointer',
+    	            dataLabels: {
+    	                enabled: false,
+    	                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+    	                style: {
+    	                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+    	                }
+    	            },
+					showInLegend: true,
+    	        }
+    	    },
+    	    series: [{
+    	        type: 'pie',
+    	        name: 'HTTP Site',
+    	        data: data["http-protocol-counts"]
+    	    }]
+    	});
+
+
+		/*
+		 * HTTPS Protocol Counts Pie
+		 */
+    	$('#https-protocol-counts').highcharts({
+    	    chart: {
+    	        plotBackgroundColor: null,
+    	        plotBorderWidth: null,
+    	        plotShadow: false,
+				margin: [25, 25, 25, 25],
+    	    },
+    	    title: {
+    	        text: 'HTTPS Site'
+    	    },
+    	    tooltip: {
+    		    pointFormat: '{series.name}: <b>{point.percentage:.1f}% ({point.y} objects)</b>'
+    	    },
+    	    plotOptions: {
+    	        pie: {
+    	            allowPointSelect: true,
+    	            cursor: 'pointer',
+    	            dataLabels: {
+    	                enabled: false,
+    	            },
+					showInLegend: true,
+    	        }
+    	    },
+    	    series: [{
+    	        type: 'pie',
+    	        name: 'HTTPS Site',
+    	        data: data["https-protocol-counts"]
+    	    }]
+    	});
+
+		/*
+		 * Object origin details
+		 */
+		var tbl_body = "";
+		$.each(data["object-details"], function() {
+		    var tbl_row = "";
+			tbl_row += "<td style=\"overflow:hidden; white-space:nowrap; \" title=\""+ this["filename"] + "\">" + this["filename"] + "</td>";
+
+			if (this.hasOwnProperty("http-origin") && this["http-origin"] != "") {
+				// Display a colored 'HTTP' or 'HTTPS' badge
+				var label = "";
+				if (this["http-protocol"] == "http")
+					label = "<span class=\"label label-default\">HTTP</span>";
+				else
+					label = "<span class=\"label label-success\">HTTPS</span>";
+				label += "&nbsp;&nbsp;&nbsp;";
+
+				tbl_row += "<td style=\"overflow:hidden; white-space:nowrap; padding-left: 20px;\" title=\""+ this["http-origin"] + "\">" + label + this["http-origin"] + "</td>";
+			} else {
+				tbl_row += "<td></td>";
+			}
+
+			if (this.hasOwnProperty("https-origin") && this["https-origin"] != "") {
+				// Display a colored 'HTTP' or 'HTTPS' badge
+				var label = "";
+				if (this["https-protocol"] == "http")
+					label = "<span class=\"label label-danger\">HTTP</span>";
+				else
+					label = "<span class=\"label label-success\">HTTPS</span>";
+				label += "&nbsp;&nbsp;&nbsp;";
+
+				tbl_row += "<td style=\"overflow:hidden; white-space:nowrap; padding-left: 20px;\" title=\""+ this["https-origin"] + "\">" + label + this["https-origin"] + "</td>";
+			} else {
+				tbl_row += "<td></td>";
+			}
+
+		    tbl_body += "<tr>"+tbl_row+"</tr>\n";
+		})
+		$("#object-details-table tbody").html(tbl_body);
+
+	});
+
+	load_object_type_breakdown('count', 'normal');
+});
---
+++ www/js/loadsummary.js
@@ -0,0 +1,257 @@
+$(function () {
+	load_basic_stats('sort-alpha');
+	load_protocol_counts('http', 'sort-alpha');
+	load_protocol_counts('https', 'sort-alpha');
+
+	$.getJSON('./alexa-top500/summary.json', function(data) {
+		/*
+		 *  Protocol availability
+		 */
+    	$('#availability-pie-chart').highcharts({
+    	    chart: {
+    	        plotBackgroundColor: null,
+    	        plotBorderWidth: null,
+    	        plotShadow: false,
+				margin: [25, 25, 25, 25],
+    	    },
+    	    title: {
+    	        text: 'HTTPS Support'
+    	    },
+    	    tooltip: {
+    		    pointFormat: '<b>{point.percentage:.1f}% ({point.y} sites)</b>'
+    	    },
+    	    plotOptions: {
+    	        pie: {
+    	            allowPointSelect: true,
+    	            cursor: 'pointer',
+    	            dataLabels: {
+    	                enabled: false,
+    	            },
+					showInLegend: true,
+    	        }
+    	    },
+    	    series: [{
+    	        type: 'pie',
+    	        data: data["availability"]
+    	    }]
+    	});
+	});
+});
+
+function load_basic_stats(sort) {
+	var basic_stats = ["num_objects", "num_tcp_handshakes", "num_mbytes", "num_hosts"];
+	basic_stats.forEach(function(stat) {
+		load_basic_stat(stat, sort);
+		load_basic_stat_diff(stat, sort);
+	});
+}
+
+function load_protocol_counts(site, sort) {
+	$.getJSON('./alexa-top500/summary.json', function(data) {
+		/*
+		 * protocol count stack plots
+		 */
+		$(function () {
+			var id = '#' + site + '-site-counts';
+			var key = site + '_site_protocol_counts';
+    	    $(id).highcharts({
+    	        chart: {
+    	            type: 'area'
+    	        },
+    	        title: {
+    	            text: site.toUpperCase() + ' Sites'
+    	        },
+    	        xAxis: {
+					categories: data[key]["url"][sort],
+    	            tickmarkPlacement: 'on',
+					labels: {
+						enabled: false
+					},
+    	            title: {
+    	                enabled: false
+    	            }
+    	        },
+    	        yAxis: {
+    	            title: {
+    	                text: 'Number of Objects'
+    	            },
+    	        },
+    	        tooltip: {
+    	            shared: true,
+    	            valueSuffix: ' objects'
+    	        },
+    	    	plotOptions: {
+    	    	    area: {
+						stacking: 'normal',
+    	    	        marker: {
+    	    	            enabled: false,
+    	    	            symbol: 'circle',
+    	    	            radius: 2,
+    	    	            states: {
+    	    	                hover: {
+    	    	                    enabled: true
+    	    	                }
+    	    	            }
+    	    	        }
+    	    	    }
+    	    	},
+    	        series: [{
+    	            name: 'HTTP Objects',
+    	            data: data[key]["HTTP"][sort]
+    	        }, {
+    	            name: 'HTTPS Objects',
+    	            data: data[key]["HTTPS"][sort]
+    	        }]
+    	    });
+    	});
+
+	});
+}
+
+
+
+var ylabels = new Array();
+ylabels["num_objects"] = "Number of Objects";
+ylabels["num_tcp_handshakes"] = "Number of TCP Connections";
+ylabels["num_mbytes"] = "Total Size (MB)";
+ylabels["num_hosts"] = "Number of Hosts";
+
+function load_basic_stat(stat, sort) {
+	$.getJSON('./alexa-top500/summary.json', function(data) {
+    	$('#'+stat).highcharts({
+    	    chart: {
+    	        type: 'area'
+    	    },
+    	    title: {
+    	        text: ylabels[stat]
+    	    },
+    	    xAxis: {
+				categories: data[stat]["url"][sort],
+    	        tickmarkPlacement: 'on',
+				labels: {
+					enabled: false
+				},
+    	        title: {
+    	            enabled: false
+    	        }
+    	    },
+    	    yAxis: {
+    	        title: {
+    	            text: ylabels[stat]
+    	        },
+    	    },
+    	    tooltip: {
+    	        shared: true,
+    	        //valueSuffix: ' objects'
+    	    },
+    	    plotOptions: {
+    	        area: {
+    	            marker: {
+    	                enabled: false,
+    	                symbol: 'circle',
+    	                radius: 2,
+    	                states: {
+    	                    hover: {
+    	                        enabled: true
+    	                    }
+    	                }
+    	            }
+    	        }
+    	    },
+    	    series: [{
+    	        name: 'HTTP',
+    	        data: data[stat]["HTTP"][sort]
+    	    }, {
+    	        name: 'HTTPS',
+    	        data: data[stat]["HTTPS"][sort]
+    	    }]
+    	});
+	});
+}
+
+/* subtract a2 from a1 element-wise */
+function subtract_arrays(a1, a2) {
+	if (a1.length != a2.length)
+		return null;
+
+	var result = new Array(a1.length);
+	for (i = 0; i < a1.length; i++) {
+		result[i] = a1[i] - a2[i];
+	}
+
+	return result;
+}
+
+function assign_threshold_colors(data, threshold) {
+	var colored_data = new Array(data.length);
+	for (i = 0; i < data.length; i++) {
+		the_color = 'gray';
+		if (data[i] > 0)
+			the_color = '#3BA686';
+		else if (data[i] < 0)
+			the_color = '#E04946';
+
+		colored_data[i] = {y:data[i], color:the_color};
+	}
+
+	return colored_data;
+}
+
+function load_basic_stat_diff(stat, sort) {
+	$.getJSON('./alexa-top500/summary.json', function(data) {
+    	$('#'+stat+"-diff").highcharts({
+    	    chart: {
+    	        type: 'column'
+    	    },
+    	    title: {
+    	        text: 'Increase in HTTPS Version',
+    	    },
+    	    xAxis: {
+				categories: data[stat]["url"][sort],
+    	        tickmarkPlacement: 'on',
+				labels: {
+					enabled: false
+				},
+    	        title: {
+    	            enabled: false
+    	        }
+    	    },
+    	    yAxis: {
+    	        title: {
+    	            text: ylabels[stat]
+    	        },
+    	    },
+    	    tooltip: {
+				shared:'true',
+                formatter:function(a,b,c){
+					var s = '';
+					$.each(this.points, function(i, point) {
+                    	s += '<b>HTTPS-HTTP:</b> ' + point.y;
+					});
+                    return s;
+                },
+    	    },
+			legend: {
+				enabled: false,
+			},
+    	    plotOptions: {
+    	        area: {
+    	            marker: {
+    	                enabled: false,
+    	                symbol: 'circle',
+    	                radius: 2,
+    	                states: {
+    	                    hover: {
+    	                        enabled: true
+    	                    }
+    	                }
+    	            }
+    	        }
+    	    },
+    	    series: [{
+    	        name: 'HTTPS-HTTP',
+    	        data: assign_threshold_colors(subtract_arrays(data[stat]["HTTPS"][sort], data[stat]["HTTP"][sort]), 0),
+    	    }]
+    	});
+	});
+}
