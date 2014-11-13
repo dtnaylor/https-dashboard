040d18a7f3273b4b179aa747cab613cf33d7dead
David Naylor <dnaylor@cs.cmu.edu>
2014-11-13 17:32:53 -0500

    Moved manager configuration to external conf file

--- manager.py
+++ manager.py
@@ -29,50 +29,68 @@ PROFILER = './profiler.py'
 RSYNC = '/usr/bin/env rsync'
 
 
-# configuration
-# TODO: put these in conf file
-MANAGER_LOG='./logs/manager.log'
-HAR_GENERATOR_LOG='./logs/har_generator.log'
-SCREENSHOT_GENERATOR_LOG='./logs/screenshot_generator.log'
-PROFILER_LOG='./logs/profiler.log'
 
-URL_FILE=None #'./web-profiler/tools/tmp_urls'
-USER_AGENTS={
-    'chrome-37-osx':
-       {'name': 'Chrome 37 (OSX)', 'string': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36'},
-    'chrome-18-android':
-       {'name': 'Chrome 18 (Android)', 'string': 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'},
-}
-#USER_AGENTS={'default': 
-#                {'name': 'Default', 'string': None},
-#}
-TEMPDIR=os.path.join(tempfile.gettempdir(), 'https-dashboard')
-OUTDIR='./profiles'
-#WEB_SERVER='linux.gp.cs.cmu.edu'
-WEB_SERVER='linuxgp'
-WEB_SERVER_DIR='/afs/cs.cmu.edu/project/httpsdashboard/www'
+def setup_logging():
+    if args.quiet:
+        level = logging.WARNING
+    elif args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    config = {
+        'filename': conf['MANAGER_LOG'],
+        'format' : "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
+        'level' : level
+    }
+    logging.basicConfig(**config)
 
-OUT_SUBDIR = None
+    # email me on error or exception
+    smtp_conf = None
+    with open(conf['SMTP_CONF'], 'r') as f:
+        smtp_conf = eval(f.read())
+    f.closed
+
+    email_handler = handlers.SMTPHandler(\
+        smtp_conf['server'], 'dtbn07@gmail.com',\
+        ['dtbn07@gmail.com'], 'HTTPS Dashboard Error',\
+        credentials=smtp_conf['credentials'], secure=())
+    email_handler.setLevel(logging.ERROR)
+    logging.getLogger('').addHandler(email_handler)
+
+
+def load_conf(conf_file):
+    conf = None
+    try:
+        with open(conf_file, 'r') as f:
+            conf = eval(f.read())
+        f.closed
+    except:
+        logging.exception('Error reading configuration: %s', conf_file)
+
+    return conf
+
 
 
 def main():
-    global URL_FILE
     logging.info('=============== MANAGER LAUNCHED ===============')
 
     ##
     ## Prepare temp directories
     ##
     try:
-        if os.path.exists(TEMPDIR):
+        if not conf['TEMPDIR']:
+            conf['TEMPDIR'] = os.path.join(tempfile.gettempdir(), 'https-dashboard')
+
+        if os.path.exists(conf['TEMPDIR']):
             logging.info('Removing existing temp directory')
-            shutil.rmtree(TEMPDIR)
-        os.makedirs(TEMPDIR)
+            shutil.rmtree(conf['TEMPDIR'])
+        os.makedirs(conf['TEMPDIR'])
 
         # make a subdir for each user agent
-        for user_agent_tag in USER_AGENTS:
-            os.makedirs(os.path.join(TEMPDIR, user_agent_tag))
+        for user_agent_tag in conf['USER_AGENTS']:
+            os.makedirs(os.path.join(conf['TEMPDIR'], user_agent_tag))
 
-        logging.info('Set up temp directory: %s', TEMPDIR)
+        logging.info('Set up temp directory: %s', conf['TEMPDIR'])
     except:
         logging.exception('Error preparing temp directory')
         sys.exit(-1)
@@ -85,29 +103,29 @@ def main():
     try:
         # make new directory in outdir named with today's date
         today = datetime.datetime.now().strftime('%Y-%m-%d')
-        OUT_SUBDIR = os.path.abspath(os.path.join(OUTDIR, today))
-        if os.path.exists(OUT_SUBDIR):
+        conf['OUT_SUBDIR'] = os.path.abspath(os.path.join(conf['OUTDIR'], today))
+        if os.path.exists(conf['OUT_SUBDIR']):
             backup_dir = '%s_backup-%s' %\
-                (OUT_SUBDIR, datetime.datetime.now().strftime('%H-%M-%S'))
+                (conf['OUT_SUBDIR'], datetime.datetime.now().strftime('%H-%M-%S'))
             logging.warn('Subdirectory for today already exists; moving to: %s', backup_dir)
-            shutil.move(OUT_SUBDIR, backup_dir)
-        os.makedirs(OUT_SUBDIR)
+            shutil.move(conf['OUT_SUBDIR'], backup_dir)
+        os.makedirs(conf['OUT_SUBDIR'])
         
         # make a subdir for each user agent
-        for user_agent_tag in USER_AGENTS:
-            os.makedirs(os.path.join(OUT_SUBDIR, user_agent_tag))
+        for user_agent_tag in conf['USER_AGENTS']:
+            os.makedirs(os.path.join(conf['OUT_SUBDIR'], user_agent_tag))
 
         # write manifest file
         # TODO: add crawl date
         manifest = {
-            'user-agents': USER_AGENTS,
+            'user-agents': conf['USER_AGENTS'],
         }
-        manifest_file = os.path.join(OUT_SUBDIR, 'crawl-manifest.json')
+        manifest_file = os.path.join(conf['OUT_SUBDIR'], 'crawl-manifest.json')
         with open(manifest_file, 'w') as f:
             json.dump(manifest, f)
         f.closed
 
-        logging.info('Set up output subdirectory: %s', OUT_SUBDIR)
+        logging.info('Set up output subdirectory: %s', conf['OUT_SUBDIR'])
     except:
         logging.exception('Error preparing output directory')
         sys.exit(-1)
@@ -117,10 +135,10 @@ def main():
     ##
     ## Prepare URL list
     ##
-    if not URL_FILE:
+    if not conf['URL_FILE']:
         try:
-            alexa_url_list = '%s/alexa_url_list.txt' % TEMPDIR
-            prepared_url_list = '%s/prepared_url_list.txt' % TEMPDIR
+            alexa_url_list = '%s/alexa_url_list.txt' % conf['TEMPDIR']
+            prepared_url_list = '%s/prepared_url_list.txt' % conf['TEMPDIR']
 
             # Get top 500 Alexa URLs
             # TODO: top 500
@@ -134,7 +152,7 @@ def main():
             logging.info('Preparing URL list: %s' % prepare_cmd)
             subprocess.check_call(prepare_cmd.split())
 
-            URL_FILE = prepared_url_list
+            conf['URL_FILE'] = prepared_url_list
         except:
             logging.exception('Error preparing URL list')
             sys.exit(-1)
@@ -143,21 +161,21 @@ def main():
     ##
     ## Generate profiles
     ##
-    for user_agent_tag in USER_AGENTS:
+    for user_agent_tag in conf['USER_AGENTS']:
         logging.info('Generating profiles for user agent: %s' % user_agent_tag)
         try:
 
             ##
             ## STAGE ONE: Generate HARs and screenshots for the URLs
             ##
-            uagent_tmpdir = os.path.join(TEMPDIR, user_agent_tag)
+            uagent_tmpdir = os.path.join(conf['TEMPDIR'], user_agent_tag)
             har_cmd = '%s -f %s -o %s -g %s -v' %\
-                (HAR_GENERATOR, URL_FILE, uagent_tmpdir, HAR_GENERATOR_LOG)
+                (HAR_GENERATOR, conf['URL_FILE'], uagent_tmpdir, conf['HAR_GENERATOR_LOG'])
             screenshot_cmd = '%s -f %s -o %s -g %s -v' %\
-                (SCREENSHOT_GENERATOR, URL_FILE, uagent_tmpdir, SCREENSHOT_GENERATOR_LOG)
-            if USER_AGENTS[user_agent_tag]['string']:
-                har_cmd += ' -u "%s"' % USER_AGENTS[user_agent_tag]['string']
-                screenshot_cmd += ' -u "%s"' % USER_AGENTS[user_agent_tag]['string']
+                (SCREENSHOT_GENERATOR, conf['URL_FILE'], uagent_tmpdir, conf['SCREENSHOT_GENERATOR_LOG'])
+            if conf['USER_AGENTS'][user_agent_tag]['string']:
+                har_cmd += ' -u "%s"' % conf['USER_AGENTS'][user_agent_tag]['string']
+                screenshot_cmd += ' -u "%s"' % conf['USER_AGENTS'][user_agent_tag]['string']
             logging.debug('Running HAR genrator: %s', har_cmd)
             subprocess.check_call(har_cmd, shell=True)  # TODO: careful!
             logging.debug('Running screenshot genrator: %s', screenshot_cmd)
@@ -167,9 +185,9 @@ def main():
             ##
             ## STAGE TWO: Generate profiles
             ##
-            uagent_outdir = os.path.join(OUT_SUBDIR, user_agent_tag)
+            uagent_outdir = os.path.join(conf['OUT_SUBDIR'], user_agent_tag)
             profiler_cmd = '%s -d %s -o %s -g %s -v' %\
-                (PROFILER, uagent_tmpdir, uagent_outdir, PROFILER_LOG)
+                (PROFILER, uagent_tmpdir, uagent_outdir, conf['PROFILER_LOG'])
             logging.debug('Running profiler: %s', profiler_cmd)
             subprocess.check_call(profiler_cmd.split())
 
@@ -187,14 +205,14 @@ def main():
     ##
     ## Delete temp directories
     ##
-    #shutil.rmtree(TEMPDIR)
+    #shutil.rmtree(conf['TEMPDIR'])
 
     ##
     ## If successful, update main manifest
     ##
     # TODO: only update if everything was OK?
     try:
-        main_manifest_file = os.path.join(OUTDIR, 'main-manifest.json')
+        main_manifest_file = os.path.join(conf['OUTDIR'], 'main-manifest.json')
         if os.path.exists(main_manifest_file):
             with open(main_manifest_file, 'r') as f:
                 main_manifest = json.load(f)
@@ -217,10 +235,9 @@ def main():
     ##
     ## Copy files to web server
     ##
-    # TODO: test
     try:
         rsync_cmd = '%s -avz --delete %s %s:%s' %\
-            (RSYNC, OUTDIR, WEB_SERVER, WEB_SERVER_DIR)
+            (RSYNC, conf['OUTDIR'], conf['WEB_SERVER'], conf['WEB_SERVER_DIR'])
         logging.debug('Running rsync: %s', rsync_cmd)
         subprocess.check_call(rsync_cmd.split())
     except:
@@ -234,41 +251,20 @@ def main():
 if __name__ == "__main__":
     # set up command line args
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
-                                     description='Compare two HARs.')
+                                     description='Manage all the stages of site crawling for HTTPS Dashboard.')
     parser.add_argument('-c', '--config', default='./default.conf', help='Manager configuration file')
     parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
     #parser.add_argument('-g', '--logfile', default=None, help='Path for log file.')
     args = parser.parse_args()
     
+
+
+    # load conf
+    conf = load_conf(args.config)
+
+
     # set up logging
-    if args.quiet:
-        level = logging.WARNING
-    elif args.verbose:
-        level = logging.DEBUG
-    else:
-        level = logging.INFO
-    config = {
-        'filename': MANAGER_LOG,
-        'format' : "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
-        'level' : level
-    }
-    logging.basicConfig(**config)
-
-    # email me on error or exception
-    # TODO: cleanup
-    smtp_conf = None
-    with open('smtp.conf', 'r') as f:
-        data = f.read()
-        smtp_conf = eval(data)
-    f.closed
-
-    email_handler = handlers.SMTPHandler(\
-        smtp_conf['server'], 'dtbn07@gmail.com',\
-        ['dtbn07@gmail.com'], 'HTTPS Dashboard Error',\
-        credentials=smtp_conf['credentials'], secure=())
-    email_handler.setLevel(logging.ERROR)
-    logging.getLogger('').addHandler(email_handler)
-
+    setup_logging()
     
     main()
