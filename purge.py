84cae4c870008328f4e181c4711caadd06a9f7b3
David Naylor <dnaylor@cs.cmu.edu>
2014-12-07 23:54:41 -0500

    Purge old screenshots

---
+++ purge.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+##
+## Purges old crawl data (e.g., old screenshots)
+##
+
+import os
+import sys
+import argparse
+import logging
+import glob
+
+
+def setup_logging():
+    if args.quiet:
+        level = logging.WARNING
+    elif args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    config = {
+        'format' : "%(levelname) -10s %(asctime)s %(module)s:%(lineno) -7s %(message)s",
+        'level' : level
+    }
+    logging.basicConfig(**config)
+
+
+
+
+
+def purge_screenshots(profile_dir, num_to_keep):
+    '''Remove screenshots for all crawls except the most recent num_to_keep.
+       Does not remove thumbnails.'''
+
+    # get the non-backup crawl directories
+    crawl_dirs = glob.glob(profile_dir + '/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]')
+    crawl_dirs = sorted(crawl_dirs, reverse=True)
+
+    # remove screenshots from all but the newest num_to_keep
+    for crawl_dir in crawl_dirs[num_to_keep:]:
+        for screenshot in [img for img in\
+            glob.glob('%s/*/site_screenshots/*.png' % crawl_dir)\
+            if 'thumb' not in img]:                      # filter out thumbnails
+
+            try:
+                logging.debug('Removing %s', screenshot)
+                os.remove(screenshot)
+            except:
+                logging.exception('Error removing %s', screenshot)
+
+
+
+
+
+def main():
+    if args.screenshots:
+        purge_screenshots(args.profiles, args.keep)
+
+
+
+
+
+
+if __name__ == "__main__":
+    # set up command line args
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,\
+                                     description='Purge old crawl data (e.g., old screenshots).')
+    parser.add_argument('profiles', nargs='?', default='./profiles', help='Profile directory.')
+    parser.add_argument('-s', '--screenshots', action='store_true', default=False, help='Purge screenshots.')
+    parser.add_argument('-k', '--keep', type=int, default=7, help='Don\'t purge the most recent \'keep\' crawls.')
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='only print errors')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print debug info. --quiet wins if both are present')
+    args = parser.parse_args()
+    
+
+    # set up logging
+    setup_logging()
+    
+    main()
